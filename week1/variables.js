65a313281481fc9b12c8f77f0c7e16b36a1ac934
jswithalex <alexanderramsdell@gmail.com>
2015-06-04 17:30:28 -0400

    Update variables.js

--- week1/variables.js
+++ week1/variables.js
@@ -1,6 +1,16 @@
-/* Variables, Datatypes, Expressions, Statements */
+/* Variables, Datatypes, Expressions, Statements, Functions */
 
 /*
+  
+*/
+
+/*
+
+  VARIABLES
+  
+  What is a variable?  
+  Simply put, it's a name that points to the place in memory where a value is kept.  You pick a useful name to store some value and the program retains that name.  When you want to use that value, you use the variable name you gave it. Note: a variable does not have quotes around it (that's a string). 
+  
   to declare a variable, you need:
   
   the 'var' keyword
@@ -17,3 +27,33 @@ var age = 23;
 var ofVotingAge = true;
 
 console.log(fullName);
+/* 
+  prints: Bill,Gates
+  By the way: console.log is a function. It takes in any number of values and prints them to the browser console.
+*/
+
+/* 
+
+EXPRESSIONS 
+
+An expression could look like this:
+
+1 + 2
+
+or this:
+
+'Bill' + ' ' + 'Gates'
+
+or this:
+
+42
+
+or this:
+
+(42 + 'a').length
+
+Expressions consist of one or more elements that are reduced to a single value.
+
+
+*/
+
