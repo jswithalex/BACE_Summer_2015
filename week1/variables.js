d3b55b6c580297db57ac6d0b79569ed2aef55bd3
jswithalex <alexanderramsdell@gmail.com>
2015-06-04 16:02:08 -0400

    Create variables.js

---
+++ week1/variables.js
@@ -0,0 +1,17 @@
+/* Variables, Datatypes, Expressions, Statements */
+
+/*
+  to declare a variable, you need:
+  
+  the 'var' keyword
+  the variable name
+  a single '=' sign
+  a value
+  a semicolon (tells the javascript interpreter that the statement is over)
+*/
+
+var firstName = 'Bill';
+var lastName = 'Gates';
+var fullName = firstName + ',' + lastName;
+var age = 23;
+var ofVotingAge = true;
