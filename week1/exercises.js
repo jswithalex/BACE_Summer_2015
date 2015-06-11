64d8078457f2f9d9dbbfe2e9f92b8d655b7d1b63
jswithalex <alexanderramsdell@gmail.com>
2015-06-10 23:31:54 -0400

    Create exercises.js

---
+++ week1/exercises.js
@@ -0,0 +1,49 @@
+// 
+//
+// VARIABLES
+//
+// a variable stores a value under a name 
+//
+//
+
+var user_age   =  24;
+var user_first =  'Mike';
+var user_last  =  'Jordan';
+
+//
+// LOGGING VALUES 
+//
+// you can print one or more values with console.log()
+//
+//
+
+console.log(user_age);
+
+// EXERCISE 1: log the values of user_first and user_last
+// EXERCISE 2: add the first and last names with +
+// EXERCISE 3: log twice the user's age
+
+
+
+//
+//
+// CONTROL FLOW
+//
+
+
+var voting_age = 18;
+
+if (user_age > voting_age) {
+
+  console.log(user_first,'can vote.');
+
+}
+
+else {
+
+// EXERCISE 4: log a message that the user can't vote.
+
+}
+
+
+
