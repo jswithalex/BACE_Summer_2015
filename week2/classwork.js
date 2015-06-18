5886613b1b6fa1f6ad4f4c8c629ae2a01b73235a
jswithalex <alexanderramsdell@gmail.com>
2015-06-18 19:42:12 -0400

    Create classwork.js

---
+++ week2/classwork.js
@@ -0,0 +1,16 @@
+// 12 C to ? F
+var c = 12;
+var f = (c * 9)/5 + 32
+
+
+// 98 F to ? C
+var f = 98;
+var c = (98 - 32) * (5/9);
+
+span for fahr is 180deg.
+
+span for celcius is 0 to 100deg;
+
+1.8 fahr degrees for each celcius.
+
+celcius freezing is 0;
