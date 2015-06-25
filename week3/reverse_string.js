3d295a22cd1e8c6f4be1a31ab334ed736d399654
jswithalex <alexanderramsdell@gmail.com>
2015-06-25 13:46:52 -0400

    Update reverse_string.js

--- week3/reverse_string.js
+++ week3/reverse_string.js
@@ -1,9 +1,9 @@
 function reverse_string(s) {
   
+  // This function runs forever, why?
   var s_array = s.split(''); 
   var reversed_string = '';
   while(s_array) {
-    console.log(s_array);
     reversed_string += s_array.pop();
   } 
 
