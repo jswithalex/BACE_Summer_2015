c571124bd12ca1cacdf475abf0dc68e2da324b11
jswithalex <alexanderramsdell@gmail.com>
2015-06-25 14:06:50 -0400

    Update functions.js

--- week3/functions.js
+++ week3/functions.js
@@ -2,12 +2,15 @@
 
 // Functions are groups of statments
 
-// Why Functions? 
+// Why Functions? Let's see what Crockford has to say
 // https://www.youtube.com/watch?v=ya4UHuXNygM
-// 20:26
+// 19:13 - 21:30
 
 
 // Here are some built-in functions that we don't have to write
+// .split()
+// math.pow()
+// array.pop()
 
 
 // split a string into an array
@@ -17,9 +20,11 @@ var s2.split();
 
 // filter out the odd numbers from a list
 var nums = [1,4,5,7,2];
-nums.filter(function(num) {
- return num % 2 === 1; // is odd;
-});
+nums.reverse();
+// nums is now [2,7,5,4,1]
+nums.sort()
+// [1,2,4,5,7]
+
 
 
 // First We Define a Function
@@ -44,5 +49,16 @@ function d(func,
 
 var d = [1,2,3,4,5];
 
-var e = d.reduce(function(item) { return item % 2 === 0 ? item : null} )
+function isEven(item) { return item%2 === 0 }
+var e = d.reduce(isEven);
 console.log(e);
+
+function double(item){
+   return item*2;
+}
+
+var f = d.map(double);
+
+// IN CLASS INDIVIDUAL EXERCISES 
+
+// http://eloquentjavascript.net/02_program_structure.html#h_TcUD2vzyMe
