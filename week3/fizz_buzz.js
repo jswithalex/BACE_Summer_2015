231342914f234f7fe807d9ad98a0c23a583cb5dc
jswithalex <alexanderramsdell@gmail.com>
2015-06-25 13:51:04 -0400

    Update fizz_buzz.js

--- week3/fizz_buzz.js
+++ week3/fizz_buzz.js
@@ -1,5 +1,8 @@
+// My Fizz Buzz Solution
+
+
 var i;
-for (i=0;i<101;i++) {
+for (i=0; i<101; i++) {
   if (i % 15 === 0) {
     console.log('buzz');
   }
@@ -14,3 +17,7 @@ for (i=0;i<101;i++) {
     console.log(i);
   }
 }
+
+// Exercise: turn this into a function that prints 'fizz' if a number is divisible by 3, 'buzz' if a number is 
+// divisible by 5, and 'fizzbuzz' if a number is divisible by 15.  The catch is that the function should accept 
+// a variable called max which should determine the upper limit of the fizz buzz function.
