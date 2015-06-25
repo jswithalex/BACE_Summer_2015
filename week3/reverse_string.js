d84db49f047a36f99f0fefe83cdabda08dff1bd5
jswithalex <alexanderramsdell@gmail.com>
2015-06-25 13:47:28 -0400

    Update reverse_string.js

--- week3/reverse_string.js
+++ week3/reverse_string.js
@@ -1,11 +1,14 @@
+// This function will never terminate and thus will crash your browser.  Why?
+
 function reverse_string(s) {
-  
-  // This function runs forever, why?
+
   var s_array = s.split(''); 
   var reversed_string = '';
+
   while(s_array) {
     reversed_string += s_array.pop();
   } 
 
   return reversed_string;
 }
+
